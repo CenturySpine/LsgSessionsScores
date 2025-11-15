2f5180600901867473aa40142ba7e83a5d42124e
CenturySpine <century.spine@gmail.com>
2025-11-15 18:47:24 +0100

    chore(release): 1.0.4 (code 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         minSdk = 29
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
